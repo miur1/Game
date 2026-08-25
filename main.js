51e0482af2ce799b37a00b38cf8e16878e6fb198
miur1 <miureal121@gmail.com>
2026-08-25 21:56:23 +0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+import kaboom from "https://unpkg.com/kaboom@3000.0.1/dist/kaboom.mjs";
+
+kaboom({ background: [34, 34, 34] });
+
+// --- VARIABEL ATRIBUT GAME ---
+let expCount = 0;
+let level = 1;
+// ... (dan seterusnya sampai kodenya habis di bagian lanjutkanGame() ) ...
+
